f4f720264c4c458e24410c28c9d0f98787f94d43
sarsha <smatan84@gmail.com>
2016-02-29 11:21:16 +0200

    removed old dialog files

--- src/app/newRecipeDialog/newRecipeDialog.module.js
+++
@@ -1,7 +0,0 @@
-var angular = require('angular');
-var service = require('./newRecipeDialog.service');
-
-require('./newRecipeDialog.css');
-
-module.exports = angular.module('recipesBook.newRecipeDialog', [])
-  .service('rbDialog', service);
